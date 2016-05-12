adf553d7a08ef1125e8721af72670834e22d7dfd
Mikael Reiersølmoen <mikaelr@ntnu.no>
2016-05-12 18:26:55 +0200

    Added cookie to toplist-call (caused unauthorized exception)

--- app/src/main/java/no/ntnu/mikaelr/delta/async_task/GetTopListAsyncTask.java
+++ app/src/main/java/no/ntnu/mikaelr/delta/async_task/GetTopListAsyncTask.java
@@ -3,6 +3,7 @@ package no.ntnu.mikaelr.delta.async_task;
 import android.os.AsyncTask;
 import android.support.v4.util.Pair;
 import no.ntnu.mikaelr.delta.interactor.ProjectInteractorImpl;
+import no.ntnu.mikaelr.delta.util.SharedPrefsUtil;
 import no.ntnu.mikaelr.delta.util.StatusCode;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -34,6 +35,7 @@ public class GetTopListAsyncTask extends AsyncTask<Void, Void, Pair<Integer, Res
         ((SimpleClientHttpRequestFactory) template.getRequestFactory()).setConnectTimeout(1000 * 10);
         HttpHeaders headers = new HttpHeaders();
         headers.set("Content-type", "application/json; charset=utf-8");
+        headers.set("Cookie", SharedPrefsUtil.getInstance().getCookie());
         HttpEntity<String> entity = new HttpEntity<String>(headers);
 
         try {
